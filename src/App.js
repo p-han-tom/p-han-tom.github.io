626d02b8b7ee0e5f6a4d1d819578ac035f203d62
p-han-tom <hantom0127@gmail.com>
2021-06-29 11:27:45 -0400

    Profile picture

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import profilepic from "./images/Yearbook1.jpg";
+import { createMuiTheme } from '@material-ui/core/styles';
 import './App.css';
+import { BackgroundColor } from 'jest-matcher-utils/node_modules/chalk';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#9be7ff',
+      main: '#64b5f6',
+      dark: '#2286c3',
+      contrastText: '#000',
+    },
+    secondary: {
+      light: '#ffad42',
+      main: '#f57c00',
+      dark: '#bb4d00',
+      contrastText: '#000',
+    },
+  },
+});
+
+const colors = theme.palette;
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Homepage">
+      <Avatar alt="Tom Han"
+        className="profilepic"
+        src={profilepic}
+        style={{
+          width: "250px",
+          height: "250px",
+          border:  "5px solid",
+          borderColor: colors.primary.dark
+          }
+        } />
+      <h1>Tom Han</h1>
     </div>
   );
 }
